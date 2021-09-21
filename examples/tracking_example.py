b3b498eb565c9f790896640aaadc9d6c7365303c
Kleckner Lab <dkleckner@ucmerced.edu>
2021-09-21 15:48:43 -0700

    Tracking example complete!

--- examples/tracking_example.py
+++ examples/tracking_example.py
@@ -19,88 +19,168 @@ An example which generates a volume with a distorted cube of points
 '''
 
 import sys
-from muvi import VolumetricMovie, VolumeProperties
+from muvi import VolumetricMovie, VolumeProperties, open_3D_movie
 from muvi.distortion import get_distortion_model
 import numpy as np
 import time
 from scipy import ndimage
 import os
 from scipy.spatial.transform import Rotation
+import pickle
+from scipy.interpolate import UnivariateSpline
 
-W = 128
-N = 128
+GAUSSIAN_SIZE = 2
+FRAMES = 60
+DT = 1 / FRAMES
+
+def mag(X):
+    return np.sqrt((X**2).sum(-1))
 
 output_dir = "tracking_example"
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-info = VolumeProperties(
-    Lx = 70,
-    Ly = 90,
-    Lz = 100,
-    Nx = 64,
-    Ny = 64,
-    Nz = 128,
-    dx = 75,
-    dz = 100
-)
+# Our sample points are a cube, which is then rotated.
+cube = 5 + 20 * ((np.arange(8).reshape(-1, 1) // 2**np.arange(3)) % 2 - 0.5).astype('f')
 
-indices = np.mgrid[:info['Nx'], :info['Ny'], :info['Nz']].T # Tranposing changes shape to [Nz, Ny, Nx, 3]
-distortion = get_distortion_model(info)
-X = distortion.convert(indices, 'index-xyz', 'physical')
+vec = np.array([1, 2, 4], 'f')
+vec *= 2 * np.pi / mag(vec)
 
-def gaussians(X0, X, w=1):
-    data = np.zeros(X.shape[:-1], 'd')
-    for x0 in X0:
-        data += np.exp(-((X - x0)**2).sum(-1) / w**2)
-    return data
+print(mag(vec))
 
-cube = 30 * ((np.arange(8).reshape(-1, 1) // 2**np.arange(3)) % 2 - 0.5).astype('f')
+rot_cube = [
+    Rotation.from_rotvec(n / FRAMES * vec).apply(cube)
+        for n in range(FRAMES)]
 
-frame = np.clip(gaussians(cube, X, 2) * 200, 0, 255).astype('u1')
+# MUVI file name
+mfn = os.path.join(output_dir, "tracking.vti")
+if not os.path.exists(mfn):
+    print(f'Generating volumetric movie: {mfn}')
 
-VolumetricMovie([frame], info).save(os.path.join(output_dir, "tracking.vti"))
+    # Generate a properties object so we can get a distortion object
+    # Note that by specifying dx/dz, which are introducing distortion, which
+    #   is quite severe in this case.  Try setting the distortion corection
+    #   factors to 0 in the viewer to see how warped the raw data is!
+    info = VolumeProperties(
+        Lx = 70,
+        Ly = 90,
+        Lz = 100,
+        Nx = 64,
+        Ny = 64,
+        Nz = 128,
+        dx = 75,
+        dz = 100
+    )
 
-sys.exit()
+    # Create a grid of indices, which correspond to the voxel indices...
+    indices = np.mgrid[:info['Nx'], :info['Ny'], :info['Nz']].T # Tranposing changes shape to [Nz, Ny, Nx, 3]
+    # ... and convert this to physical positions in the distorted volume.
+    distortion = get_distortion_model(info)
+    X = distortion.convert(indices, 'index-xyz', 'physical')
 
-if len(sys.argv) == 1:
-    fn = 'gyroid.vti'
+    # Each point will be a Gaussian blob
+    def gaussians(X0, X, w=1):
+        data = np.zeros(X.shape[:-1], 'd')
+        for x0 in X0:
+            data += np.exp(-((X - x0)**2).sum(-1) / w**2)
+        return data
+
+    # Generate the frames
+    frames = [np.clip(gaussians(r_cube, X, GAUSSIAN_SIZE) * 200, 0, 255).astype('u1') for r_cube in rot_cube]
+
+    # ... and save them!
+    VolumetricMovie(frames, info).save(mfn)
 else:
-    fn = sys.argv[1]
+    print(f'Found volumetric movie: {mfn}')
+    print('Skipping creation; delete file to regenerate...')
 
 
-start = time.time()
-scale = 2*np.pi/W
+dfn = os.path.join(output_dir, 'tracking.pickle')
+if not os.path.exists(dfn):
+    print("Tracking points...")
+    import trackpy as tp
+    # Open the 3D movie
+    vol = open_3D_movie(mfn)
 
-x = np.arange(W).reshape(1, 1, -1) * scale
-y = np.arange(W).reshape(1, -1, 1) * scale
-z = np.arange(W).reshape(-1, 1, 1) * scale
+    # Identify the points in the volume
+    data = tp.batch(vol, 5)
+    # Note: the second parameter is the size in the frame.  In this case,
+    #   5 is approximately right, but you would need to tweak for a different
+    #   data set!
 
-gyroid_base = np.sin(x)*np.cos(y) + np.sin(y)*np.cos(z) + np.sin(z)*np.cos(x)
-noise = 5*ndimage.gaussian_filter((np.random.rand(W, W, W)-0.5) * 1.0, 3, mode='wrap')
+    # Link the particles into tracks
+    data = tp.link(data, 10, memory=3)
+    print(f'Found {data.particle.max()+1} particle tracks')
 
-np_frames = [
-    (255 * np.clip(np.roll(gyroid_base, n, axis=0) + noise - 0.5, 0, 1)).astype('u1').reshape(W, W, W, 1) for n in range(N)
-]
+    # Find the physical coordinates.  These will appear as the columns "xc", "yc",
+    # and "zc" in the data frame *after* you run this command, and will be in the
+    # physical units of the data (i.e. L and not N)
+    vol.distortion.update_data_frame(data)
 
-el = time.time() - start
+    # Save to a pickle file
+    with open(dfn, "wb") as f:
+        pickle.dump(data, f)
+else:
+    print(f"Found existing track data, delete {dfn} to regenerate...")
 
-print('Generate volume: %.1f ms' % (el * 1000))
-print('Fraction empty: %.1f%%' % ((np_frames[0]==0).mean()*100))
 
+with open(dfn, "rb") as f:
+    data = pickle.load(f)
 
-info = {
-    'Lx': 100,
-    'Ly': 100,
-    'Lz': 100,
-    'units': 'mm',
-}
+# Let's make a movie where each point is a sphere!
+pfn = os.path.join(output_dir, 'tracking_points_frame%d.ply')
+from muvi.mesh import generate_glyphs
 
-start = time.time()
-VolumetricMovie(np_frames, **info).save(fn)
-el = time.time() - start
+for i in range(FRAMES):
+    points = np.array(data[data.frame == i][["xc", "yc", "zc"]])
+    generate_glyphs(points, "sphere", a=GAUSSIAN_SIZE*1.5).save(pfn % i)
 
-fs = os.path.getsize(fn)
-rs = N * np_frames[0].nbytes
-print('Save compressed file: %.1f ms (%.2f raw GB/s)' % (el*1000, rs/1E9/el))
-print('Final size: %d MB (compression ratio=%.2f)' % (fs/1E6, rs/fs))
+# Let's make a movie with vector arrows for each point
+vfn = os.path.join(output_dir, 'tracking_vel_frame%d.ply')
+
+# First we need to construct a trajectory for each particle track, which
+#   we'll do with UnivariateSplines.
+# This is a lot to keep track of, so we'll define a class for this!
+# Note: err is the expected error for each particle location.  Setting this
+#   higher will result in a smoother trajectory -- note that the output
+#   locations from the trajectories will *not* exactly match the input positions
+#   (unless you set s=0, which you probably shouldn't!)
+class Trajectories:
+    def __init__(self, data, dt=1.0, err=0.25, min_length=3, columns=["xc", "yc", "zc"]):
+        self.tracks = []
+
+        for i in range(data.particle.max() + 1):
+            track = data[data.particle == i]
+            if len(track) < min_length:
+                continue
+
+            t = np.array(track['frame'] * dt)
+
+            items = [(t.min(), t.max())]
+            X = []
+            V = []
+
+            for column in columns:
+                spline = UnivariateSpline(t, np.array(track[column]), s=len(t) * err**2)
+                X.append(spline)
+                V.append(spline.derivative())
+
+            self.tracks.append(((t.min(), t.max()), tuple(X), tuple(V)))
+
+    def __call__(self, t):
+        X = []
+        V = []
+        for limits, XS, VS in self.tracks:
+            if t >= limits[0] and t <= limits[1]:
+                X.append([S(t) for S in XS])
+                V.append([S(t) for S in VS])
+
+        return np.array(X), np.array(V)
+
+# Build the trajactory object
+traj = Trajectories(data, dt=DT)
+
+# And make the frames!
+for i in range(FRAMES):
+    X, V = traj(i * DT)
+    generate_glyphs(X, "tick", a=GAUSSIAN_SIZE*5, N=V, color=mag(V), clim=(0, 150)).save(vfn % i)
