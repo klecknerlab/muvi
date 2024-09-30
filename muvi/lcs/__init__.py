eb877db4fdf94686de5f446e35953260edf33c37
tapiasilvadiego <dts195@gmail.com>
2024-09-30 13:05:31 -0700

    updated init file to throw away unesccesary data

--- muvi/lcs/__init__.py
+++ muvi/lcs/__init__.py
@@ -19,6 +19,7 @@ from scipy.interpolate import RBFInterpolator
 import pandas as pd
 #Developed modules
 from muvi.geometry.resample import windowed_polynomial_resample
+from muvi.lcs.particle_tracking_filters import adj_frames_filter
 
 class FTLEs:
     '''
@@ -29,75 +30,7 @@ class FTLEs:
     def __init__(self):
         pass
     
-    def adj_frames_filter(self, data, start_frame):
-        """
-        -Filters data (either pandas data frame) keeping only particles that appear in two adjacent frames.
-        This is used particularly for particle tracking data because particles can dissapear and reappear between frames.
 
-        Args:
-            data: pandas data frame (e.g. from trackpy). 
-            start_frame: starting frame for filtering.
-        Returns:
-            Filtered trajectories
-        """
-        if isinstance(data, pd.DataFrame):
-            #Determine end_frame 
-            end_frame = start_frame + 1
-
-            #Define frames to be considered
-            frames = np.arange(start_frame, end_frame + 1)
-            filtered_data = data[data['frame'].isin(frames)]
-            df_adj_frames = filtered_data[filtered_data['frame'].isin(frames)]
-
-            #Identify particles and their frame counts
-            particle_frame_counts = df_adj_frames.groupby('particle')['frame'].nunique()
-            valid_particles = particle_frame_counts[particle_frame_counts == len(frames)].index
-
-            #Filter duplicates to keep only those particles appearing in all frames
-            duplicates = df_adj_frames[df_adj_frames['particle'].isin(valid_particles)]
-
-            corrected_coords = ['xc', 'yc', 'zc']
-            uncorrected_coords = ['x', 'y', 'z']
-
-            #Find existing corrected coordinates
-            existing_corrected = [coord for coord in corrected_coords if coord in data.columns]
-
-            #If at least one corrected coordinate is found, use it
-            if existing_corrected:
-                coords = existing_corrected
-            else:
-                # Find existing uncorrected coordinates if no corrected coordinates are found
-                existing_uncorrected = [coord for coord in uncorrected_coords if coord in data.columns]
-                if existing_uncorrected:
-                    coords = existing_uncorrected
-                else:
-                    raise ValueError("Neither corrected ('xc', 'yc', 'zc') nor uncorrected ('x', 'y', 'z') coordinates are present in the DataFrame.")
-            
-            num_dims = len(coords)
-
-            if num_dims <2:
-                raise ValueError(f"num_dims must be either 2 or 3. Current num_dims: {num_dims}. With coordinates: {coords}")
-
-            #Check if preferred coordinates are present
-            coords = corrected_coords if all(coord in data.columns for coord in corrected_coords) else uncorrected_coords
-
-            #Extract and sort trajectories
-            particles0 = duplicates[duplicates['frame'] == start_frame]['particle']
-            particles1 = duplicates[duplicates['frame'] == start_frame + 1]['particle']
-            traj0 = duplicates[duplicates['frame'] == start_frame][coords].to_numpy()
-            traj1 = duplicates[duplicates['frame'] == start_frame + 1][coords].to_numpy()
-
-            sorted_indices0 = particles0.argsort()
-            sorted_indices1 = particles1.argsort()
-
-            traj0 = traj0[sorted_indices0]
-            traj1 = traj1[sorted_indices1]
-
-        else:
-            raise ValueError(f"data must be a pandas data frame which results from particle tracking (e.g. trackpy).")
-
-        return np.stack((traj0, traj1), axis=0)
-    
     def rk4_step(self, p0, func, t, dt):
         K1 = dt * func(p0, t)
         K2 = dt * func(p0 + 0.5*K1, t + 0.5*dt)
@@ -107,100 +40,107 @@ class FTLEs:
         return p0 + (1/6) * (K1 + 2*K2 + 2*K3 + K4)
     
     def RK4(self, p0, func, t, dt):
-        sol = np.zeros((len(t),) + p0.shape)
-
-        for i, t in enumerate(t):
-            sol[i] = p0
-            p0 = self.rk4_step(p0, func, t, dt)
-
+        sol = []
+        for t_ in t:
+            sol.append(p0)
+            p0 = self.rk4_step(p0, func, t_, dt)
+        
         return sol
     
-    def fmc_step(self, traj, R, interpolator = 'rbf', **interp_params):
-        #Interpolate between the small time flow maps, f, onto the discrete grid denoted by R.
-        if interpolator == 'rbf':
-            f_step = RBFInterpolator(traj[0], traj[1], **interp_params)(R.reshape(-1, R.shape[-1])).reshape(R.shape)
+    def fmc_step(self, traj, R, interpolator='rbf', **interp_params):
+        interpolators = ['rbf', 'windowed_polynomial']
+        if interpolator not in interpolators:
+            raise ValueError(f"Select interpolator 'rbf' or 'windowed_polynomial'.")
         
-        elif interpolator == 'windowed polynomial':
-            f_step = windowed_polynomial_resample(traj[0], traj[1], **interp_params)(R.reshape(-1, R.shape[-1])).reshape(R.shape)
+        if interpolator == interpolators[0]:
+            f_step = RBFInterpolator(traj[0], traj[1], **interp_params)(R.reshape(-1, R.shape[-1])).reshape(R.shape)
+            return f_step
         
         else:
-            print("Please choose interpolator 'rbf' or 'windowed polynomial'.")
+            #Using interpolator 'windowed polynomial' allows us to obtain the gradient for free
+            f_step = windowed_polynomial_resample(R, traj[0], traj[1], **interp_params)[0]
             
-        return f_step
+            return f_step
     
-    def FMC(self, R, pickle_fn=None, traj=None, interpolator='rbf', **interp_params):
+    def FMC(self, data, R, interpolator='rbf', **interp_params):
         """
         -Flow map compiliation method to compute flow map. 
         -Supports particle tracking data, particles will dissapear and appear from frame to frame, through the 
         adj_frame_filter function.
         Args:
             R: Interpolation grid.
-            pickle_fn (optional): Path to the pickle file which has data stored in a dataframe. Default is None.
-            traj (optional): Trajectory data. Default is None.
+            data: DataFrame (typically resulting from trackpy) or data store as a numpy array.
             interpolator: Type of interpolator to use. Default is 'rbf'.
             **interp_params: Additional parameters for interpolation.
         Returns:
-            Flow map
+            Flow map or Gradient of flow map.
+            Note: If interpolator is set to 'windowed polynomial' we obtain the GRADIENT of the FLOW MAP for FREE!
         """
         print(f"Computing flow maps using flow map compiliation (FMC)...")
-        
-        if pickle_fn is not None:
-            data = pd.read_pickle(pickle_fn)
-            end_frame = data['frame'].nunique()
-            
-            print(f"Processing with pickle file: {pickle_fn}")
-            frames = np.arange(0, end_frame)
-
+        if  isinstance(data, pd.DataFrame):
+            print(f"Processing with pandas DataFrame.")
+            end_frame = data['frame'].max()
+            start_frame = data['frame'].min()
+            frames = np.arange(start_frame, end_frame)
             particle_tracking = True
-
+            
         else:
-            print("Processing with provided trajectory data.")
-            data = traj
+            print("Processing with provided numpy array.")
             frames = np.arange(0, data.shape[0])
             particle_tracking = False
 
 
         f0 = np.expand_dims(R, axis=0)
-        f = np.zeros(((len(frames) - 1,) + R.shape))
-        
+        #f = np.zeros(((len(frames) - 1,) + R.shape))
         for i, frame in enumerate(frames[:-1]): #frames[:-1] because fmc_step uses the i and the i+1 entry
-            print(f"Flowmap: {i+1}/{len(frames)-1}...")
+            print(f"Flow map: {i+1}/{len(frames)-1}...")
 
             if particle_tracking == True:
-                traj = self.adj_frames_filter(data, frame)
-                f_step = self.fmc_step(traj, R, interpolator, **interp_params)
+                traj = adj_frames_filter(data, frame)
+                result = self.fmc_step(traj, R, interpolator, **interp_params)
+                
+            else:
+                traj = data
+                result = self.fmc_step(traj[i:i+2], R, interpolator, **interp_params)
+
+            if interpolator == 'windowed_polynomial':
+                f_step = result #G is the derivative of the windowed polynomial fit
+                nan_mask = np.isnan(f_step)
+                print(f"Number of NaN values in f_step: {np.sum(nan_mask)}")
+                f_step[nan_mask] = 0
 
             else:
-                f_step = self.fmc_step(traj[i:i+2], R, interpolator, **interp_params)
+                f_step = result
             
             R = f_step   
-            f[i, ...] = f_step
+            #f[i, ...] = f_step
         
-        print(f"F shape: {np.vstack((f0, f)).shape}")
-
-        return np.vstack((f0, f))
+        return np.array([f0, f_step])
+        
+    
     
     def vfi_step(self, pos_traj, vel_traj, R, dt, interpolator ='rbf', **interp_params):
-        if interpolator == 'rbf':
+        interpolators = ['rbf', 'windowed_polynomial']
+        if interpolator not in interpolators:
+            raise ValueError(f"Select interpolator 'rbf' or 'windowed_polynomial'.")
+    
+        if interpolator == interpolators[0]:
             K1 = dt * RBFInterpolator(pos_traj[0, ...], vel_traj[0, ...], **interp_params)(R.reshape(-1, 2)).reshape(R.shape)
             K2 = dt * RBFInterpolator(pos_traj[1,...], vel_traj[1,...], **interp_params)((R + dt*K1).reshape(-1, 2)).reshape(R.shape)
             K3 = dt * RBFInterpolator(pos_traj[1,...], vel_traj[1,...], **interp_params)((R + dt*K2).reshape(-1, 2)).reshape(R.shape)
             K4 = dt * RBFInterpolator(pos_traj[2,...], vel_traj[2,...], **interp_params)((R + 2*dt*K3).reshape(-1, 2)).reshape(R.shape)
         
-        elif interpolator == 'windowed polynomial':
-            K1 = dt * windowed_polynomial_resample(pos_traj[0], vel_traj[0], **interp_params)(R.reshape(-1, R.shape[-1])).reshape(R.shape)
-            K2 = dt * windowed_polynomial_resample(pos_traj[1,...], vel_traj[1,...], **interp_params)((R + dt*K1).reshape(-1, 2)).reshape(R.shape)
-            K3 = dt * windowed_polynomial_resample(pos_traj[1,...], vel_traj[1,...], **interp_params)((R + dt*K2).reshape(-1, 2)).reshape(R.shape)
-            K4 = dt * windowed_polynomial_resample(pos_traj[2,...], vel_traj[2,...], **interp_params)((R + 2*dt*K3).reshape(-1, 2)).reshape(R.shape)
-
         else:
-            print("Please choose interpolator 'rbf' or 'windowed polynomial'.")
+            K1 = dt * windowed_polynomial_resample(R, pos_traj[0], vel_traj[0], **interp_params)[0]
+            K2 = dt * windowed_polynomial_resample(R + dt*K1, pos_traj[1,...], vel_traj[1,...], **interp_params)[0]
+            K3 = dt * windowed_polynomial_resample(R + dt*K2, pos_traj[1,...], vel_traj[1,...], **interp_params)[0]
+            K4 = dt * windowed_polynomial_resample(R + 2*dt*K3, pos_traj[2,...], vel_traj[2,...], **interp_params)[0]
         
         f_step =  (1/6) * (K1 + 2*K2 + 2*K3 + K4) + R
         
         return f_step 
         
-    def VFI(self, R, dt, pos_traj, vel_traj, interpolator='rbf', **interp_params):
+    def VFI(self, pos_traj, vel_traj, R, dt, interpolator='rbf', **interp_params):
         """
         -Velocity field integration method to compute flow map. 
         -Does NOT support particle tracking data (more speed efficient to use FMC) because particle tracking velocimetry (PTV)
@@ -225,29 +165,26 @@ class FTLEs:
         f = np.zeros(((len(frames)-3,) + R.shape)) #len(frames)-3 because vfi_step uses the i, the i+1 and i+2
 
         for i in frames[:-3]:
-            print(f"Flowmap: {i+1}/{len(frames)-3}...")
+            print(f"Flow map: {i+1}/{len(frames)-3}...")
             f_step = self.vfi_step(pos_traj[i:i+3], vel_traj[i:i+3], R, dt, interpolator, **interp_params)
             R = f_step   
             f[i, ...] = f_step
-        
-        print(f"F shape: {np.vstack((f0, f)).shape}")
-        
-        return np.vstack((f0, f))
+                
+        return np.array([f0, f])
     
     
-    def grad_flowmap(self, F):
+    def grad_step(self, Ft0, FT):
         """
         -Computes gradient of flow map for arbitary number of dimensions
         Args:
-            F: Flow map.
+            Ft0: Flow map at time step t0.
+            FT: Flow map at time step T.
 
         Returns:
             Gradient of flow map
         """
-        print(f"Computing gradient of flow map")
-        #Initialize flow map, F from 0 to T.
-        Ft0 = F[0]
-        FT = F[-1]
+        print(f"Computing gradient of flow map...")
+
         G = np.zeros((Ft0.shape + (Ft0.shape[-1],)))
         #Compute number of dimension
         num_dims = Ft0.shape[-1]
@@ -262,12 +199,13 @@ class FTLEs:
                 #The following returns [delta_x1/delta_X1, delta_x2/delta_X1, delta_x1/delta_X2, delta_x2/delta_X2]
                 g.append(grad_FT[j][..., i]/grad_Ft0[(num_dims-1) - k][..., k])
 
-        #Store Jacobian of the flow field, and stores it as follows [[delta_x1/delta_X1, delta_x2/delta_X1],[delta_x1/delta_X2, delta_x2/delta_X2]]
+        #Store gradient of the flow field, and stores it as follows [[delta_x1/delta_X1, delta_x2/delta_X1],[delta_x1/delta_X2, delta_x2/delta_X2]]
         for i in range(num_dims):
             for j in range(num_dims):
                 G[..., i, j] = g[i+j]
-    
+        
         return G
+    
 
     def ftle_field(self, G, T):
         """
@@ -286,7 +224,6 @@ class FTLEs:
         for index in np.ndindex(spatial_shape):
             G_jk = G[index]  #Extract the Jacobian matrix at this spatial location
             C = np.einsum('ij...,jk...->ik...', G_jk.T, G_jk)  #Compute Cauchy-Green tensor
-
             eigenvalues = LA.eigvals(C)
             max_eigenvalue = np.max(np.abs(eigenvalues))
             min_eigenvalue = np.min(np.abs(eigenvalues))
@@ -296,3 +233,4 @@ class FTLEs:
 
         return [ftles_fwd, ftles_bwd]
     
+#start from the grid and computer the flow map from the grid to 0 to h and then start off again and computer it from h to 2h
